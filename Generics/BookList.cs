014afc0af97f3bc7a78e05121b77240a14b956d2
Andrew Jones <jones.gaffney1920@gmail.com>
2017-10-20 18:38:24 -0500

    Add project files.

---
+++ Generics/BookList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+
+    public class ObjectList
+    {
+        public void Add(object value)
+        {
+
+        }
+        public object this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class GenericDictionaryByAJ<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book { Isbn = "1111", Title = "C# Advanced" };
+
+            //var numbers = new List();
+            //numbers.Add(10);
+
+            //var books = new BookList();
+            //books.Add(book);
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            var books = new GenericList<Book>();
+            books.Add(book);
+            books.Add(new Book());
+
+            var dictionary = new GenericDictionaryByAJ<string, Book>();
+            dictionary.Add("1234", new Book());
+
+            var discountBook = new DiscountCalculator<Book>();
+            discountBook.CalculateDiscount(new Book());
+
+            var discountProduct = new DiscountCalculator<Product>();
+            discountProduct.CalculateDiscount(new Book());
+            discountProduct.CalculateDiscount(new Product());
+
+            var number = new Nullable<int>();
+            Console.WriteLine("Has Value? " + number.HasValue);
+            Console.WriteLine("Value: " + number.GetValueOrDefault());
+
+            Console.WriteLine();
+
+            number = new Nullable<int>(5);
+            Console.WriteLine("Has Value? " + number.HasValue);
+            Console.WriteLine("Value: " + number.GetValueOrDefault());
+        }
+    }
+}
